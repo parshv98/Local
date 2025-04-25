9230bf54b4b407ddfb3d722ab90a1c1e5b627ac0
parshva <parshvabsukhadiya@gmail.com>
2025-04-25 23:32:52 +0530

    add

---
+++ jadugar.java
@@ -0,0 +1,11 @@
+
+
+public class jadugar {
+    public static void main(String[] args) {
+        System.out.println("kon hai ya java");
+        for(int i=0;i<5;i++){
+            System.out.println("hello");
+        }
+        System.out.println("hello");
+    }
+}
